2f955646c8f38064cfa1db7b0f3a2591a3cfe463
Naomi Kyoto <nakyoto@gmail.com>
2013-01-12 22:09:59 -0600

    improved colours.js; now shows all 256 colours
    deoxxa was holding out on the 232-255 colours; now they're shown in the
    palette tests

--- colours.js
+++ colours.js
@@ -2,28 +2,51 @@ var Jetty = require('./index.js');
 var tty = new Jetty(process.stdout);
 tty.reset().clear();
 
-var renderSwatch = function(r,g,b) {
-  
-  // pos
-  var x = r*8+b*2;
-  var y = g*8+b;
-  
+var render256Palette = function(r,g,b) {
+  var x = r*8+b*2, y = g*8+b;
+  renderSwatch([y,x], [r,g,b], '#'+r+g+b, r+g+b >= 7);
+};
+
+var renderGrayPalette = function(yOffset, dec) {
+  var intensity = (dec-232);
+  var x=intensity*5, y=yOffset;
+  renderSwatch([y,x], dec, dec+'d', intensity >= 12);
+};
+
+var renderSystemPalette = function(yOffset, dec) {
+  var x=dec*5, y=yOffset;
+  renderSwatch([y,x], dec, dec+'d', dec > 0);
+};
+
+var renderSwatch = function(coords, channels, label, isBlack) {
   // swatch
   for (var _x=0; _x<4; _x++) {
     for (var _y=0; _y<2; _y++) {
-      tty.moveTo([y+_y, x+_x]).rgb([r,g,b],1).text(' ');
+      tty.moveTo([coords[0]+_y, coords[1]+_x]).rgb(channels,1).text(' ');
     }
   }
 
   // label
-  tty.moveTo([y,x]).text('#'+r+g+b);
-}
+  tty.moveTo(coords).rgb(isBlack ? 0 : [5,5,5]).text(label);
+};
 
 // all the colors!
 for (var r=0; r<6; ++r) {
   for (var g=0; g<6; ++g) {
     for (var b=0; b<6; ++b) {
-      renderSwatch(r,g,b);
+      render256Palette(r,g,b);
     }
   }
 }
+
+// all the grays
+for (var k=232; k<256; k++) {
+  renderGrayPalette(50, k);
+}
+
+// all the system colors
+for (var k=0; k<16; k++) {
+  renderSystemPalette(54, k);
+}
+
+tty.moveTo([60,0]).reset();
